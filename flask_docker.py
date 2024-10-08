e6a64245ff1a2336d0bd8d4220176cc01c0152d5
quanLe1010 <quanB2105684@student.ctu.edu.vn>
2024-10-08 22:01:07 +0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -6,4 +6,4 @@ def hello_world():
     return 'Hello FOSS, Hello CI/CD using Jenkins'
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
